2a8fa7b5ad7ba1ed4d115112f67c88d54463e456
turboclaw <dan@turboclaw.com>
2014-09-11 13:59:11 -0400

    moremoremore

--- js/reveal.js
+++ js/reveal.js
@@ -19,8 +19,8 @@ var Reveal = (function(){
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 1200,
-			height: 800,
+			width: 1140,
+			height: 700,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.1,
